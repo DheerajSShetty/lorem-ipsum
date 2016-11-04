7fbb90fff0a2c51526b447df98792e7a6d4e4d90
DheerajSShetty <dheerajs@vmware.com>
2016-11-03 17:46:14 -0700

    Working

--- js/loremgen.js
+++ js/loremgen.js
@@ -1,12 +1,30 @@
-var LORIPSUM = LORIPSUM || (function(){
-    var _args = {};
+var para = ["<p>I think the big problem this country has is being politically correct. I've been challenged by so many people, and I don't frankly  have time for total political correctness. And to be honest with you, this country doesn't have time either.</p>",
+"One thing about television, it brings out personality. People are able to watch me in action. They hear my voice and see my eyes. There's nothing I can hide. That's me. Television brings out your flaws, your weaknesses, your strengths, and you truths. The audience either likes you or it doesn't.",
+"I actually don't have a bad hairline."];
 
-    return {
-        init : function(Args) {
-            _args = Args;
-        },
-        helloWorld : function() {
-            alert('Hello World! -' + _args[0]);
-        }
-    };
-}());
+var words = ["voice", "country", "ice"];
+var sentences = ["I think the big problem this country has is being politically correct", "One thing about television, it brings out personality.",
+"They hear my voice and see my eyes."];
+
+$('#ipsumForm').submit(function(event) {
+  var type;
+  var num = document.getElementById("howmany").value;
+  var html = document.getElementById("htmlneeded").checked;
+
+  if (document.getElementById("optradio1").checked)
+  {
+    $('#outputtext').val(para);  
+  }
+  else if (document.getElementById("optradio2").checked)
+  {
+    $('#outputtext').val(words);
+  }
+  else 
+  {
+    $('#outputtext').val(sentences);
+  }
+
+  event.preventDefault();
+});
+
+
